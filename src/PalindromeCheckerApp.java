63a596845a2eddb3b46c178119884afb21173b2c
rohit1911-code <rsarrsar919@gmail.com>
2026-03-12 10:21:51 +0530

    UC13:Palindrome Checker App

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -7,13 +7,22 @@ public class PalindromeCheckerApp {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Input: ");
-        String input = scanner.nextLine();
+        String input = scanner.nextLine().toLowerCase();
 
-        PalindromeStrategy strategy = new StackStrategy();
+        PalindromeStrategy stackStrategy = new StackStrategy();
+        PalindromeStrategy twoPointerStrategy = new TwoPointerStrategy();
 
-        boolean result = strategy.check(input);
+        long start1 = System.nanoTime();
+        boolean result1 = stackStrategy.check(input);
+        long end1 = System.nanoTime();
 
-        System.out.println("Is Palindrome: " + result);
+        long start2 = System.nanoTime();
+        boolean result2 = twoPointerStrategy.check(input);
+        long end2 = System.nanoTime();
+
+        System.out.println("Stack Strategy Result: " + result1);
+        System.out.println("Stack Execution Time: " + (end1 - start1) + " ns");
+
 
         scanner.close();
     }
@@ -37,6 +46,25 @@ class StackStrategy implements PalindromeStrategy {
             if (c != stack.pop()) {
                 return false;
             }
+        }
+
+        return true;
+    }
+}
+
+class TwoPointerStrategy implements PalindromeStrategy {
+
+    public boolean check(String input) {
+
+        int start = 0;
+        int end = input.length() - 1;
+
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
         }
 
         return true;
