81874378b27764107bb6cb92ee45bd30e8fb997b
rohit1911-code <rsarrsar919@gmail.com>
2026-03-12 10:12:31 +0530

    UC7:Deque-Based Optimized Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -25,6 +25,7 @@ public class PalindromeCheckerApp{
             }
         }
 
+
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
     }
