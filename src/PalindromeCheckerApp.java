649dad91fbb098161594a842718c0447576f43fe
rohit1911-code <rsarrsar919@gmail.com>
2026-03-12 10:06:02 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,8 +1,18 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args){
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version:1.0");
-        System.out.println("System initialized successfully");
+        String word = "madam";
 
+        String reversed = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed += word.charAt(i);
+        }
+
+        if (word.equals(reversed)) {
+            System.out.println("Word: " + word);
+            System.out.println("Result: It IS a palindrome.");
+        } else {
+            System.out.println("Word: " + word);
+            System.out.println("Result: It is NOT a palindrome.");
+        }
     }
 }
