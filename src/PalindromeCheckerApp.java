5fa9653c565153664ecee585ab3e0dc9bac0b7cd
rohit1911-code <rsarrsar919@gmail.com>
2026-03-12 10:15:46 +0530

    UC9:Recursive Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,17 +1,6 @@
 import java.util.Scanner;
 
-
 public class PalindromeCheckerApp {
-    static class Node {
-        char data;
-        Node next;
-
-        Node(char data) {
-            this.data = data;
-            this.next = null;
-        }
-    }
-
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -19,61 +8,24 @@ public class PalindromeCheckerApp {
         System.out.print("Input : ");
         String input = scanner.nextLine();
 
-        Node head = null, tail = null;
+        boolean result = check(input, 0, input.length() - 1);
 
-        for (int i = 0; i < input.length(); i++) {
-            Node newNode = new Node(input.charAt(i));
-
-            if (head == null) {
-                head = newNode;
-                tail = newNode;
-            } else {
-                tail.next = newNode;
-                tail = newNode;
-            }
-        }
-
-        boolean isPalindrome = isPalindrome(head);
-
-        System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println("Is Palindrome? : " + result);
 
         scanner.close();
     }
 
-    public static boolean isPalindrome(Node head) {
+    private static boolean check(String s, int start, int end) {
 
-        if (head == null || head.next == null)
+        if (start >= end) {
             return true;
-
-        Node slow = head;
-        Node fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
         }
 
-        Node prev = null;
-        Node current = slow;
-
-        while (current != null) {
-            Node nextNode = current.next;
-            current.next = prev;
-            prev = current;
-            current = nextNode;
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
         }
 
-        Node firstHalf = head;
-        Node secondHalf = prev;
+        return check(s, start + 1, end - 1);
 
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data) {
-                return false;
-            }
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-
-        return true;
     }
 }
